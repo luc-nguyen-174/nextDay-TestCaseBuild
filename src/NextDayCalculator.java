d1a7c191968b72675152270564402e207536026a
luc-nguyen'174 <louiskingvn@gmail.com>
2023-02-04 22:36:08 +0700

    update

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class NextDayCalculator {
+
+public class NextDayCalculator {
+    public static int[] getNextDay(int day, int month, int year) {
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                if (day < 29) {
+                    day++;
+                } else {
+                    day = 1;
+                    month++;
+                }
+            } else {
+                if (day < 28) {
+                    day++;
+                } else {
+                    day = 1;
+                    month++;
+                }
+            }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (day < 30) {
+                day++;
+            } else {
+                day = 1;
+                month++;
+            }
+        } else {
+            if (month < 12) {
+                if (day < 31) {
+                    day++;
+                } else {
+                    day = 1;
+                    month++;
+                }
+            } else {
+                if (day < 31) {
+                    day++;
+                } else {
+                    day = 1;
+                    month =1;
+                    year++;
+                }
+            }
+        }
+        return new int[]{day, month, year};
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
 }
--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -1,4 +1,54 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NextDayCalculatorTest {
-  
+
+    @Test
+    void testGetNextDay1() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(1, 1, 2018);
+        int[] expected = {2, 1, 2018};
+        assertArrayEquals(expected, results);
+    }
+
+    @Test
+    void testGetNextDay2() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(31, 1, 2018);
+        int[] expected = {1, 2, 2018};
+        assertArrayEquals(expected, results);
+    }
+
+    @Test
+    void testGetNextDay3() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(30, 4, 2018);
+        int[] expected = {1, 5, 2018};
+        assertArrayEquals(expected, results);
+    }
+
+    @Test
+    void testGetNextDay4() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(28, 2, 2018);
+        int[] expected = {1, 3, 2018};
+        assertArrayEquals(expected, results);
+    }
+
+    @Test
+    void testGetNextDay5() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(29, 2, 2020);
+        int[] expected = {1, 3, 2020};
+        assertArrayEquals(expected, results);
+    }
+
+    @Test
+    void testGetNextDay6() {
+        NextDayCalculator calculator = new NextDayCalculator();
+        int[] results = calculator.getNextDay(31, 12, 2018);
+        int[] expected = {1, 1, 2019};
+        assertArrayEquals(expected, results);
+    }
 }
